7ee736273ce645977af68a4e01a8aaf6bc96f5fa
MuddanaSatyaPavani <111487125+MuddanaSatyaPavani@users.noreply.github.com>
2022-11-01 19:19:52 +0530

    def prime(u):     if u==1:         return False     for i in range(2,(int(u**0.5)+1)):         if u%i==0:             return False     else:         return True m=int(input()) n=int(input()) c=0 for j in range(m,n+1):     if prime(j)==True:         c=c+1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,15 @@
-def prime(a):
-    if a==1:
+def prime(u):
+    if u==1:
         return False
-    for i in range(2,int(a**0.5)+1):
-        if a%i==0:
+    for i in range(2,(int(u**0.5)+1)):
+        if u%i==0:
             return False
     else:
         return True
-c=0
-n=int(input())
 m=int(input())
-for i in range(n,m+1):
-    if prime(i)==True:
+n=int(input())
+c=0
+for j in range(m,n+1):
+    if prime(j)==True:
         c=c+1
 print(c)
